244aebe064a316b5ac894530ac8a53c3f2128f81
hashini-sugandika <130230222+hashini-sugandika@users.noreply.github.com>
2024-01-09 14:09:00 +0530

    component setup

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,18 @@ import Camp from "./components/Camp";
 import Features from "./components/Features";
 import Guide from "./components/Guide";
 import Hero from "./components/Hero";
+import GetApp from "./components/GetApp";
 
 export default function Home() {
   return (
     <>
       <Hero /> 
       <Camp />
-      <Features />
       <Guide />
+      <Features />
+      <GetApp/>
+     
+      
     </>
  
   )
